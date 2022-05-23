ea329629ec8ef7e8a223c7fdab87214cd9102c4c
DESKTOP-NITBD9M\aboma <abo.mahroq@gmail.com>
2022-05-23 12:55:57 +0300

    update

--- app/src/main/java/com/mrq/paljobs/controller/activities/MainActivity.java
+++ app/src/main/java/com/mrq/paljobs/controller/activities/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.core.view.GravityCompat;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -13,7 +14,7 @@ import com.mrq.paljobs.R;
 import com.mrq.paljobs.databinding.ActivityMainBinding;
 import com.mrq.paljobs.helpers.BaseActivity;
 
-@SuppressLint("StaticFieldLeak")
+@SuppressLint("StaticFieldLeak,NonConstantResourceId")
 public class MainActivity extends BaseActivity {
 
     public static ActivityMainBinding binding;
@@ -38,6 +39,19 @@ public class MainActivity extends BaseActivity {
         binding.drawer.addDrawerListener(toggle);
         toggle.syncState();
         setSupportActionBar(binding.main.appbar.toolbar);
+        initNavView();
+    }
+
+    private void initNavView() {
+        binding.navView.setCheckedItem(R.id.nav_home);
+        binding.navView.setNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    break;
+            }
+            binding.drawer.close();
+            return true;
+        });
     }
 
     @Override
